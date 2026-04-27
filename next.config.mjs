49d6e1450e0a564bd5babfedc12688225d7b599f
Chandan Umesh Pai <pai00040@umn.edu>
2026-04-27 17:35:11 -0500

    fix 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/chandan-portfolio',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/chandan-portfolio/' : '',
   output: 'export',
+  basePath: '/chandan-portfolio',
   images: {
     unoptimized: true,
   },
