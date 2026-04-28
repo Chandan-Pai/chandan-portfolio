cf2b4d22ca4644ce51da7f7d0936b6b9ce0ba322
Chandan-Pai <82177852+Chandan-Pai@users.noreply.github.com>
2026-04-27 19:41:13 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/chandan-portfolio',
+  assetPrefix: '/chandan-portfolio/', // Add this line (with trailing slash)
   output: 'export',
   images: {
     unoptimized: true,
